8fd1c612901dc2d0fb6e7c23c2bb084835118124
takuchantuyoshi <da87435@gmail.com>
2019-07-14 08:33:17 +0900

    Rename TopViewController

--- ReactorKit-Sample/TopViewController.swift
+++ ReactorKit-Sample/TopViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TopViewController.swift
 //  ReactorKit-Sample
 //
 //  Created by Takuya Ohsawa on 2019/07/07.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class TopViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
